98f21ee5ad7dfbb774b02a5d56707fcb82f33d00
leesuncom <70786826+leesuncom@users.noreply.github.com>
2020-09-22 22:28:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 cd package
 git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/kenzok8/small.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
